55e2598b851e1c7bd5ee9758c17da17c15823811
tnlanh99 <tnlanh99@gmail.com>
2023-05-13 09:50:27 +0700

    add split.py

---
+++ split.py
@@ -0,0 +1,34 @@
+import os
+import numpy as np
+
+
+def split_data(data, n_part):
+    n = data.shape[0]
+    n_part = min(n_part, n)
+    n_per_part = n // n_part
+    parts = []
+    for i in range(n_part):
+        if i == n_part - 1:
+            parts.append(data[i * n_per_part:])
+        else:
+            parts.append(data[i * n_per_part:(i + 1) * n_per_part])
+    return parts
+
+
+if __name__ == "__main__":
+    n_part = 2
+    data = np.load("./data/train.npz")
+    splited_data = [{} for _ in range(n_part)]
+
+    for file in data.files:
+        print(f"=========== Processing {file} {data[file].shape} ===========")
+        parts = split_data(data[file], n_part)
+        for i in range(len(parts)):
+            splited_data[i][file] = parts[i]
+
+    os.makedirs("./splited_data", exist_ok=True)
+
+    for i in range(n_part):
+        np.savez(f"./splited_data/train_{i}.npz", **splited_data[i])
+        
+    print("Done!")
